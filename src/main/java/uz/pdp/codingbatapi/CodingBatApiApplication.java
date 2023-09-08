5cc00a263e60552db23cf5fd7a12117e45ddcd6c
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2023-09-08 13:29:59 +0500

    init

---
+++ src/main/java/uz/pdp/codingbatapi/CodingBatApiApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.codingbatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingBatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingBatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/codingbatapi/CodingBatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.codingbatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingBatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
